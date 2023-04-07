6da49365ff73cb15baf90eb73c6cf1273433d41b
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-04-07 18:59:11 +0800

    Add files via upload

--- traffic-main.js
+++ traffic-main.js
@@ -506,6 +506,7 @@ var App = {
                     if (from == "url") {
                         if (!par1) {
                             App.goToPage("home")
+                            ToastMaker("無法解析網址參數",2000,{classList:["p-1"]})
                         }
                     }
                     var TRA_Station_Data = JSON.parse(localStorage.getItem("data")).TRA.data.Stations[par1]
