3697d7e5bdefd0a34bba26b2cbad38075b420df4
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-04-30 14:57:23 +0800

    Add files via upload

--- traffic-main.js
+++ traffic-main.js
@@ -84,7 +84,7 @@ async function timeDisplay(Displaysec) {
 }
 
 function getNearBusAndBikes(loc, container, mapObject, page, rem) {
-    $("#refresh_prog").remove()
+    
 
     console.log("getNearBusAndBikes")
 
@@ -190,7 +190,7 @@ function getNearBusAndBikes(loc, container, mapObject, page, rem) {
                         console.log(BikeStationData.bikeData[i].AvailableRentBikesDetail)
                         $("#station-display-table").append(`
     
-    <tr onclick="App.goToPage('UBIKEstation',${BikeStationData.stationData[i].StationPosition.PositionLat},${BikeStationData.stationData[i].StationPosition.PositionLon})">
+    <tr onclick="$('#refresh_prog').remove();App.goToPage('UBIKEstation',${BikeStationData.stationData[i].StationPosition.PositionLat},${BikeStationData.stationData[i].StationPosition.PositionLon})">
     <td><span class="badge ${badgeClass}">${BikeStationData.stationData[i].StationName.Zh_tw.split("_")[0]}</span><br>${BikeStationData.stationData[i].StationName.Zh_tw.split("_")[1]}</td>
     <td>一般:${BikeStationData.bikeData[i].AvailableRentBikesDetail.GeneralBikes}<br>電輔:${BikeStationData.bikeData[i].AvailableRentBikesDetail.ElectricBikes}<br>空位:${BikeStationData.bikeData[i].AvailableReturnBikes}</td>
 
@@ -243,7 +243,7 @@ function getNearBusAndBikes(loc, container, mapObject, page, rem) {
                     console.log(BikeStationData.bikeData[i].AvailableRentBikesDetail)
                     $("#station-display-table").append(`
     
-    <tr onclick="App.goToPage('UBIKEstation',${BikeStationData.stationData[i].StationPosition.PositionLat},${BikeStationData.stationData[i].StationPosition.PositionLon})">
+    <tr onclick="$('#refresh_prog').remove();App.goToPage('UBIKEstation',${BikeStationData.stationData[i].StationPosition.PositionLat},${BikeStationData.stationData[i].StationPosition.PositionLon})">
     <td><span class="badge ${badgeClass}">${BikeStationData.stationData[i].StationName.Zh_tw.split("_")[0]}</span><br>${BikeStationData.stationData[i].StationName.Zh_tw.split("_")[1]}</td>
     <td>一般:${BikeStationData.bikeData[i].AvailableRentBikesDetail.GeneralBikes}<br>電輔:${BikeStationData.bikeData[i].AvailableRentBikesDetail.ElectricBikes}<br>空位:${BikeStationData.bikeData[i].AvailableReturnBikes}</td>
 
