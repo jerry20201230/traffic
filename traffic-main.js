8b7571dddac71a87406713760c48a4f346109c18
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-05-06 16:02:47 +0800

    Add files via upload

--- traffic-main.js
+++ traffic-main.js
@@ -1339,6 +1339,7 @@ var DATA = {
                 dir = 1
             }
             for(i=0;i<pars.data.length;i++){
+                console.log(pars.data[i].StopUID)
                 if(pars.data[i].Direction ==2 || pars.data[i].Direction == dir){
                     $("#"+pars.data[i].StopUID).html(time_labal.basic(pars.data[i]))
                 }
