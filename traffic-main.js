15f2951952c3996cd670fea2cc48d96509a233db
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-05-13 07:27:32 +0800

    Add files via upload

--- traffic-main.js
+++ traffic-main.js
@@ -1144,7 +1144,7 @@ var DATA = {
 
 
 
-            <table class="table">
+            <table class="table table-bordered">
   <thead>
     <tr>
       <th scope="col" colspan="2">車輛</th>
@@ -1154,15 +1154,16 @@ var DATA = {
   <tbody>
     <tr>
       <td>一般</td>
-      <td>電輔</td>
+      <td>電輔</td> 
+      <td>${pars.data[0].AvailableReturnBikes}</td>
     </tr>
     <tr>
       <td>${pars.data[0].AvailableRentBikesDetail.GeneralBikes}</td>
       <td>${pars.data[0].AvailableRentBikesDetail.ElectricBikes}</td>
-    
+    <td></td>
     </tr>
     <tr>
-      <td>${pars.data[0].AvailableReturnBikes}</td>
+     
     </tr>
   </tbody>
 </table>
